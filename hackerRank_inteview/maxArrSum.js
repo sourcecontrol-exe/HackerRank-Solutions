3883ff6da77ee504e4fc031d6b407b84a85477eb
Swetabh Subham <swetabh.subham@gmail.com>
2021-03-18 13:58:12 +0530

    this is commit

--- hackerRank_inteview/maxArrSum.js
+++ hackerRank_inteview/maxArrSum.js
@@ -1,17 +1,5 @@
 function maxArraySum(arr){
     
-    let dp = new Array(arr.length)
-    let dp2 = []
-    let max =0
-    for(var i =0;i< arr.length ;i++){
-        dp[i] = [arr[i]]
-        max =  dp[i];
-        for(var j = i+2 ; j<arr.length ;j+=i+2){
-            max += Math.max(0,arr[j])
-            dp[i].push(max)
-        }
-        dp2.push(Math.max(...dp[i]))
-    }
-    console.log(dp2)
+    
 }
- let a = maxArraySum([3,5,-7,8,10])
+ let a = maxArraySum([-2,1,3,-4,5])
