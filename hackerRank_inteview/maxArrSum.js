500dddbe53e5d8b183aadaa4c982f12b4a4430e6
Swetabh Subham <swetabh.subham@gmail.com>
2021-03-18 14:51:02 +0530

    there you go

--- hackerRank_inteview/maxArrSum.js
+++ hackerRank_inteview/maxArrSum.js
@@ -1,5 +1,11 @@
 function maxArraySum(arr){
+   let maxCurrent = [arr[0], Math.max(arr[0], arr[1])];
+   let maxGlobal = Math.max(...maxCurrent);
+   for(var i =2;i<arr.length ;i++){
+       maxCurrent[i]= Math.max(arr[i],maxCurrent[i-2]+arr[i], maxGlobal);
+       maxGlobal = Math.max(maxGlobal,maxCurrent[i]);
+   }
     
-    
+   return maxGlobal;
 }
  let a = maxArraySum([-2,1,3,-4,5])
