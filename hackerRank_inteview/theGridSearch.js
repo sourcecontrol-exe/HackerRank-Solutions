f8729a4c19e19f7bee58d30a362c4fade2bfbc4d
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-11 00:22:35 +0530

    the grind

--- hackerRank_inteview/theGridSearch.js
+++ hackerRank_inteview/theGridSearch.js
@@ -26,12 +26,38 @@ function readLine() {
 
 // Complete the gridSearch function below.
 function gridSearch(G, P) {
-    console.log(P.length, G.length);
+    let temp = 0;
+    let p1 = 0, p2 = 0;
+     let str = "";
+    for(var i = 0 ; i< G.length - P.length+1; i++){
+        for(var j =0; j<G[i].length - P[0].length+1; j++){
+            str = G[i].slice(j, j+P[0].length)
+            p1 = i+1;
+            temp = 0;
+            if(str == P[temp]){
+                p1 = i+1;
+                temp++;
+                while(p1<G.length && temp <=P.length){
+                    if(G[p1].slice(j , j+P[temp].length) == P[temp]){
+                        console.log(G[p1].slice(j , j+P[0].length),P[temp])
+                         p1++;
+                         temp++;
+                         if(temp==P.length){
+                            return "YES";
+                         }
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return "NO";
 }
 
 function main() {
     
-
     const t = parseInt(readLine(), 10);
 
     for (let tItr = 0; tItr < t; tItr++) {
@@ -44,7 +70,7 @@ function main() {
         let G = [];
 
         for (let i = 0; i < R; i++) {
-            const GItem = readLine().split(" ");
+            const GItem = readLine();
             G.push(GItem);
         }
 
@@ -57,12 +83,12 @@ function main() {
         let P = [];
 
         for (let i = 0; i < r; i++) {
-            const PItem = readLine().split(" ");
+            const PItem = readLine();
             P.push(PItem);
         }
 
         let result = gridSearch(G, P);
-
+        console.log(result)
         
     }
 
