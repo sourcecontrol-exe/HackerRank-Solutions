08d5166746b065e24332db8134efbb537e88f5aa
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-23 17:24:49 +0530

    new

--- equalizeArray.js
+++ equalizeArray.js
@@ -3,13 +3,15 @@ function equalizeArray(arr) {
 
     for(let i =0;i<arr.length ; i++){
         if( set[arr[i]] == undefined){
-            set[arr[i]] == 1
+            set[arr[i]] = 1
         }
         else{
-            set[arr[i]] +=1;
+            set[arr[i]]+=1;
         }
     }
 
-    let max = Math.max(...Object.values(set));
+    let max = Math.max(...Object.values(set))
     return arr.length - max
 }
+
+console.log(equalizeArray([1,2,2,3]))
