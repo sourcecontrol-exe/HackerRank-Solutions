047a43b829825983809966c93fbf77928e802661
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-26 02:04:16 +0530

    minPath

--- leetcode/minPath.js
+++ leetcode/minPath.js
@@ -1,18 +1,25 @@
-var travler= function(grid,m,n){
-    if(m==1 && n ==1) return grid[m-1][n-1];
-    if(m< 1 || n< 1) return 0;
+var travler = function(grid,m,n,set = {}){
+    
+    let key = m+","+n;
+    
+    if(m ==1 && n ==1) return grid[m-1][n-1];
 
-    let result = Math.min(travler(grid, m-1,n), travler(grid, m,n-1))
-    return result;
+    if(m<1 || n<1) return Number.MAX_SAFE_INTEGER;
+    
+    if(key in set) return set[key]
+    
+    set[key] = grid[m-1][n-1] + Math.min(travler(grid,m-1,n, set),travler(grid,m,n-1,set))
+    
+    return set[key]
 }
 
 
-var minPathSum = function(irid) {
+var minPathSum = function(grid) {
     let m = grid.length;
-    let n = grid[grid.length].length;
+    let n = grid[grid.length-1].length;
     let min = travler(grid,m,n)
     return min;
 };
 
-console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+console.log(minPathSum([[1,2,3],[4,5,6]]))
 
