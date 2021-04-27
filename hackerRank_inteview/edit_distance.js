a6c1367d6499429469708a347b101ca8df28e977
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-28 03:42:17 +0530

    new

---
+++ hackerRank_inteview/edit_distance.js
@@ -0,0 +1,19 @@
+var minDistance = function(word1, word2) {
+
+};
+
+console .log(minDistance('intention', 'execution'));
+
+// horse _> ros 
+//     I N T E N T I O N
+// E   0 0 0 1 0 0 0 0 0
+// X   0 0 0 0 0 0 0 0 0
+// E   0 0 0 1 0 0 0 0 0
+// C   0 0 0 0 0 0 0 0 0
+// U   0 0 0 0 0 0 0 0 0 
+// T   0 0 1 0 0 1 0 0 0
+// I   0 0 0 0 0 0 1 0 0 
+// O   0 0 0 0 0 0 0 1 0
+// N   0 0 0 0 0 0 0 0 1
+//                                                                                                                                                                                                                             
+// 
---
+++ hackerRank_inteview/validPlaindrome.js
@@ -0,0 +1,7 @@
+var isPalindrome = function(s) {
+    
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+// console.log(isPalindrome("race a car"));
+
