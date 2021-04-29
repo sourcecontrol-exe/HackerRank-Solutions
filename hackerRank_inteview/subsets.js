cda25d9e6b148b551ed8e24c01bd148abf7e4702
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-30 00:46:01 +0530

    new

--- hackerRank_inteview/subsets.js
+++ hackerRank_inteview/subsets.js
@@ -1,15 +1,11 @@
 var subsets = function(nums) {
-  var res = [];
-    function dfs(nums, index, path, res){
-        res.push([...path]);
-        for(var i=index; i<nums.length; i+=1){
-            path.push(nums[i]);
-            dfs(nums, i+1, path, res);
-            path.pop();
-        }
-    }
-    dfs(nums, 0, [], res);
-    return res; 
+ let res = [[]]
+	for (let i = 0; i < nums.length; i++) {
+		res.forEach(item => {
+			res.push([...item, nums[i]])
+		})
+	}
+	return res
 };
 
 console.log(subsets([1,2,3]))
