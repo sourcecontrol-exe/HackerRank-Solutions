0dab90b4b991a690beeed8d1c8519baf8123d8cc
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-08 20:32:31 +0530

    not able to solve

---
+++ jumpGame4.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+function helper(arr,set,index=0, step=0){
+    
+
+}
+
+var minJumps = function(arr) {
+    let set = {};
+    for(let i = arr.length-1;i>=0 ;i--){
+        if(set[arr[i]] == undefined){
+            set[arr[i]] = arr.length-1 - i;
+        }
+    }
+    console.log(arr,set)
+    let a = helper(arr,set);
+    return a;
+};
+
+console.log(minJumps([100,-23,-23,404,100,23,23,23,3,404]))
