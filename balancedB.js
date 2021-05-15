a103fd1a77d8aab5a068f39b60ae102a1aa2c924
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-15 23:24:50 +0530

    new

--- balancedB.js
+++ balancedB.js
@@ -1,5 +1,20 @@
 function isBalanced(s) {
-    
-     
+
+    let rev = [];
+   
+    for(let i =0; i < s.length; i++){
+        
+        if((s[i] ==")" && rev[rev.length-1]=="(")
+                ||  (s[i] =="]" && rev[rev.length-1]=="[") || 
+        (s[i] =="}" && rev[rev.length-1]=="{")){
+            rev.pop();
+        }
+        else{
+            rev.push(s[i]);
+        }
+    }
+    if(rev.length == 0 ) return "YES"
+    else return "NO"
 }
-console.log(isBalanced('{(([])[])[]}[]'))
+
+console.log(isBalanced("{(([])[])[]]}"));
