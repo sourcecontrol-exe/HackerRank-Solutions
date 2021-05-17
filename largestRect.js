f53a48b5397780ef110e8483586e7111abb76727
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-17 16:20:09 +0530

    new

--- largestRect.js
+++ largestRect.js
@@ -26,20 +26,31 @@ function readLine() {
  * The function is expected to return a LONG_INTEGER.
  * The function accepts INTEGER_ARRAY h as parameter.
  */
+function rect(start, tail, k,arr){
+    return k*(Math.min(...arr.slice(start,tail)));
+}
+
 
 function largestRectangle(h) {
-   let tail = h.length;
-   let head = 0;
-   let k = h.length;
-   let max = 0;
+    let maxarea = h.length;
 
-   while(head<tail){
-       max = Math.max(max, k*(Math.min(...h.slice(head,tail))));
-       head++;
-       k--;
-       console.log(max)
-   }
-   return max
+    h.forEach((element,index) => {
+        let i = index+1;
+        let j = index-1;
+        
+        let tempArea = element;
+
+        while(i < h.length && h[i]>=element){
+            tempArea+=element;
+            i++;
+        }
+        while(j>=0 && h[j]>= element){
+            tempArea+=element;
+            j--;
+        }
+        maxarea = Math.max(tempArea, maxarea);
+    });
+    return maxarea;
 }
 
 function main() {
