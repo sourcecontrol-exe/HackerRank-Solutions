2f890ee0ccd4eda2a0ae1367b45a430ab8bc063d
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-21 23:23:34 +0530

    commit

--- leetcode/roman.js
+++ leetcode/roman.js
@@ -1,4 +1,48 @@
-var romanToInt = function(s) {
+
+
+var romanToInt = function(s){
+    let set ={
     
+        'I':1,
+        'II':2,
+        "III":3,
+        "IV":4,
+        "V":5,
+        "VI":6,
+        "VII":7,
+        "VIII":8,
+        "IX":9,
+        "X":10,
+        "XL":40,
+        "L":50, 
+        "XC":90,
+        "C":100,
+        "CD":400,
+        'D': 500,
+        "CM":900,
+        'M' :1000
+    }
+  let symbols = Object.keys(set)
+  symbols= symbols.reverse();
+    
+  let head=0;
+  let tail = s.length;
+  let temp = s.length-1;
+  let result = []
+  while(head<tail){
+    let v = s.slice(head,temp+1);
+    console.log(v)
+    if(v in set){
+        result.push(set[v].toString())
+        head = temp+1;
+        temp= s.length-1;
+    }
+    else{
+        temp--;
+    }
+  }
+
+  return result;
+
 };
-console.log(romanToInt())
+console.log(romanToInt('MMMCM'))
