31f03637baf657cb5d9deb0c9f79e1df853b3cba
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-26 23:48:23 +0530

    origin

--- leetcode/candy.js
+++ leetcode/candy.js
@@ -1,17 +1,20 @@
 var candy = function(ratings) {
-    let candies = new Array.fill(ratings.length).fill(1);
+    let candies = new Array(ratings.length).fill(1);
     
-    for(let i =0; i < ratings.length-1; i++){
-        if(ratings[i] >ratings[i+1]){
-            candies[i]++;
+    for(let i=1; i <=ratings.length-1; i++){
+        if(ratings[i]>ratings[i-1]){
+            candies[i]=candies[i-1]+1;
         }
     }
-    console.log(candies);
-    for(let j = ratings.length;j>=0;j++){
-        if(ratings[j]<=ratings[j-1]){
-            candies[i]--;
+    for(let j = ratings.length-2;j>=0;j--){
+        if(ratings[j]>ratings[j+1]){
+            candies[j] = Math.max(candies[j], candies[j+1]+1);
         }
     }
-    console.log(candies)
+    let temp =0;
+    candies.map(ele=>{
+        temp+=ele;
+    })
+    return temp;
 };
 console.log(candy( [1,2,2]))
