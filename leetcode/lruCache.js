0a16e472cadc3cc390690601b52920d554efc2a7
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-31 22:49:23 +0530

    new

---
+++ leetcode/lruCache.js
@@ -0,0 +1,59 @@
+/*
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+Follow up:
+Could you do get and put in O(1) time complexity?
+
+*/
+
+var LRUCache = function(capacity) {
+    this.cache=new Map();
+    this.cap = capacity;
+    this.index = 0;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(this.cache[key] !== undefined){
+        let ans = this.cache[key]
+        let temp = Object.entries(this.cache);
+        let index = temp.indexOf([key,ans]);
+        temp.splice(index,1);
+        this.cache = Object.fromEntries(temp);
+        this.cache[key] = ans;
+        return this.cache[key]
+    }
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if(this.cache.size==this.cap){
+        let temp = Object.entries(this.cache);
+        temp.shift();
+        this.cache = Object.fromEntries(temp);
+    }
+    this.cache[key]= value;
+};
+
+var lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+console.log(lRUCache.get(1));    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+console.log(lRUCache.get(2));    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+console.log(lRUCache.get(1));    // return -1 (not found)
+console.log(lRUCache.get(3));    // return 3
+console.log(lRUCache.get(4));    // return 4
