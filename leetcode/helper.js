9dac025bae6448424b48cac7b611222bd0a3cddf
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-02 22:59:32 +0530

    new

--- leetcode/helper.js
+++ leetcode/helper.js
@@ -2,15 +2,14 @@ function add(n1,n2) {
     let carry = 0;
     let sum = 0;
     let result = '';
-    for (var itr = 0; itr < n1.length; itr++) {
-        if (!n2[itr]) {
-            n2[itr] = '0';
+    for (var itr = 0; itr <n1.length; itr++) {
+        if (n2[itr] == undefined) {
+            n2+='0';
         }
-        console.log(n2[itr])
         sum = parseInt(n1[itr])+ parseInt(n2[itr]) + carry;
         result+= sum % 10;
         carry = Math.floor(sum/10)>0?Math.floor(sum/10):0;
-        console.log(result,sum,carry)
+        
     }
     console.log(result);
 }
